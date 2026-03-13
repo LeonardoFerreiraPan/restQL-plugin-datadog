82b8f5199625ae16329d4a94e372be51ab20ec5e
Leonardo 'alemax' Ferreira <leonardo.ferreira@mosaico.com.br>
2026-03-13 14:18:08 -0300

    feat: Add data dog start

--- main.go
+++ main.go
@@ -42,59 +42,59 @@ func txnName(method string, reqUrl *url.URL) string {
 
 func (n *DataDogPlugin) BeforeTransaction(ctx context.Context, tr restql.TransactionRequest) context.Context {
 
-	operationName := txnName(tr.Method, tr.Url)
+	// operationName := txnName(tr.Method, tr.Url)
 
-	options := []tracer.StartSpanOption{
-		tracer.Tag("name", operationName),
-		tracer.Tag(ext.SpanType, ext.SpanTypeWeb),
-		tracer.Tag(ext.HTTPMethod, tr.Method),
-		tracer.Tag(ext.HTTPURL, tr.Url),
-		tracer.Tag("_dd.measured", 1),
-	}
+	// options := []tracer.StartSpanOption{
+	// 	tracer.Tag("name", operationName),
+	// 	tracer.Tag(ext.SpanType, ext.SpanTypeWeb),
+	// 	tracer.Tag(ext.HTTPMethod, tr.Method),
+	// 	tracer.Tag(ext.HTTPURL, tr.Url),
+	// 	tracer.Tag("_dd.measured", 1),
+	// }
 
-	span := tracer.StartSpan(operationName, options...)
+	// span := tracer.StartSpan(operationName, options...)
 
-	if tr.Url.Host != "" {
-		options = append(options, tracer.Tag("http.host", tr.Url.Host))
-	}
+	// if tr.Url.Host != "" {
+	// 	options = append(options, tracer.Tag("http.host", tr.Url.Host))
+	// }
 
-	ctx = tracer.ContextWithSpan(ctx, span)
-	fmt.Println("BeforeTransaction", tr.Url, tr.Method)
+	// ctx = tracer.ContextWithSpan(ctx, span)
+	// fmt.Println("BeforeTransaction", tr.Url, tr.Method)
 
 	return ctx
 }
 
 func (n *DataDogPlugin) AfterTransaction(ctx context.Context, tr restql.TransactionResponse) context.Context {
 
-	span, ok := tracer.SpanFromContext(ctx)
-	if !ok {
-		return ctx
-	}
+	// span, ok := tracer.SpanFromContext(ctx)
+	// if !ok {
+	// 	return ctx
+	// }
 
-	var statusStr string
+	// var statusStr string
 
-	// if status is 0, treat it like 200 unless 0 was called out in DD_TRACE_HTTP_SERVER_ERROR_STATUSES
-	if tr.Status == 0 {
-		statusStr = "200"
-	} else {
-		statusStr = strconv.Itoa(tr.Status)
-		span.SetTag(ext.ErrorNoStackTrace, fmt.Errorf("%s: %s", statusStr, http.StatusText(tr.Status)))
-	}
+	// // if status is 0, treat it like 200 unless 0 was called out in DD_TRACE_HTTP_SERVER_ERROR_STATUSES
+	// if tr.Status == 0 {
+	// 	statusStr = "200"
+	// } else {
+	// 	statusStr = strconv.Itoa(tr.Status)
+	// 	span.SetTag(ext.ErrorNoStackTrace, fmt.Errorf("%s: %s", statusStr, http.StatusText(tr.Status)))
+	// }
 
-	options := []tracer.FinishOption{}
+	// options := []tracer.FinishOption{}
 
-	fc := &tracer.FinishConfig{}
-	for _, opt := range options {
-		if opt == nil {
-			continue
-		}
-		opt(fc)
-	}
+	// fc := &tracer.FinishConfig{}
+	// for _, opt := range options {
+	// 	if opt == nil {
+	// 		continue
+	// 	}
+	// 	opt(fc)
+	// }
 
-	span.SetTag(ext.HTTPCode, statusStr)
-	span.Finish(tracer.WithFinishConfig(fc))
+	// span.SetTag(ext.HTTPCode, statusStr)
+	// span.Finish(tracer.WithFinishConfig(fc))
 
-	fmt.Println("AfterTransaction", tr.Status, tr.Body)
+	// fmt.Println("AfterTransaction", tr.Status, tr.Body)
 
 	return ctx
 }
@@ -108,25 +108,25 @@ func (n *DataDogPlugin) AfterQuery(ctx context.Context, query string, result map
 
 func (n *DataDogPlugin) BeforeRequest(ctx context.Context, request restql.HTTPRequest) context.Context {
 
-	operationName := request.Method + " " + request.Path
+	// operationName := request.Method + " " + request.Path
 
-	options := []tracer.StartSpanOption{
-		tracer.Tag("name", operationName),
-		tracer.Tag(ext.SpanType, ext.SpanTypeWeb),
-		tracer.Tag(ext.HTTPMethod, request.Method),
-		tracer.Tag(ext.HTTPURL, request.Path),
-		tracer.Tag("_dd.measured", 1),
-	}
+	// options := []tracer.StartSpanOption{
+	// 	tracer.Tag("name", operationName),
+	// 	tracer.Tag(ext.SpanType, ext.SpanTypeWeb),
+	// 	tracer.Tag(ext.HTTPMethod, request.Method),
+	// 	tracer.Tag(ext.HTTPURL, request.Path),
+	// 	tracer.Tag("_dd.measured", 1),
+	// }
 
-	span := tracer.StartSpan(operationName, options...)
+	// span := tracer.StartSpan(operationName, options...)
 
-	if request.Host != "" {
-		options = append(options, tracer.Tag("http.host", request.Host))
-	}
+	// if request.Host != "" {
+	// 	options = append(options, tracer.Tag("http.host", request.Host))
+	// }
 
-	ctx = tracer.ContextWithSpan(ctx, span)
+	// ctx = tracer.ContextWithSpan(ctx, span)
 
-	fmt.Println("BeforeRequest", request.Path, request.Host, request.Body)
+	// fmt.Println("BeforeRequest", request.Path, request.Host, request.Body)
 
 	return ctx
 }
